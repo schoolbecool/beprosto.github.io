b5fe51cbd7d24d6cbe221d626782938bb9641205
schoolbecool <57950843+schoolbecool@users.noreply.github.com>
2019-11-19 14:46:13 +0100

    Add files via upload

--- gl/renderer.js
+++ gl/renderer.js
@@ -5,6 +5,8 @@ function SGR_clear(r, g, b, a) {
 	gl.clearColor(r, g, b, a);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 }
-function SGR_draw(triangleCount) {
-	gl.drawArrays(gl.TRIANGLES, 0, triangleCount);
+function SGR_draw(vertexBuffer, shader) {
+	shader.bind();
+	vertexBuffer.bind();
+	gl.drawArrays(gl.TRIANGLES, 0, vertexBuffer.i_VB_dataLength / vertexBuffer.i_VBL_fullStride);
 }
--- gl/vertexBuffer.js
+++ gl/vertexBuffer.js
@@ -4,6 +4,12 @@ function SGR_createVertexBuffer() {
   self.ID = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, self.ID);
 
+  self.i_VBL_stride = 0;
+  self.i_VBL_fullStride = 0;
+  self.i_VBL_elementCount = 0;
+  
+  self.i_VB_dataLength = 0;
+
   self.bind = function() {
     gl.bindBuffer(gl.ARRAY_BUFFER, self.ID);
     return self;
@@ -11,6 +17,20 @@ function SGR_createVertexBuffer() {
   self.sendData = function(data) {
     gl.bindBuffer(gl.ARRAY_BUFFER, self.ID);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+    self.i_VB_dataLength = data.length;
+    return self;
+  }
+  self.vertexLayoutInit = function(vertexElementCount) {
+    self.i_VBL_fullStride = vertexElementCount;
+    return self;
+  }
+  self.vertexLayoutSplit = function(count) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, self.ID);
+    gl.vertexAttribPointer(self.i_VBL_elementCount, count, gl.FLOAT, false, self.i_VBL_fullStride * 4, self.i_VBL_stride * 4); 
+    gl.enableVertexAttribArray(self.i_VBL_elementCount);
+
+    self.i_VBL_stride += count;
+    self.i_VBL_elementCount++;
     return self;
   }
 
